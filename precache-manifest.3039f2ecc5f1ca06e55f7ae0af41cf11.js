6202f61414e1f51d04266104285401120c75f69f
sidhik4742 <sidhik64742@gmail.com>
2020-07-24 20:45:55 +0530

    Updates

---
+++ precache-manifest.3039f2ecc5f1ca06e55f7ae0af41cf11.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6864a4aaf765cd5cf7835df45efed116",
+    "url": "/login-template/index.html"
+  },
+  {
+    "revision": "b57bfa47061f164829a1",
+    "url": "/login-template/static/css/main.4b627db1.chunk.css"
+  },
+  {
+    "revision": "2063aca658ba578437d7",
+    "url": "/login-template/static/js/2.06610b51.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/login-template/static/js/2.06610b51.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b57bfa47061f164829a1",
+    "url": "/login-template/static/js/main.7b5107d9.chunk.js"
+  },
+  {
+    "revision": "10628cb7505b2e6dffeb",
+    "url": "/login-template/static/js/runtime-main.447e9dae.js"
+  }
+]);
